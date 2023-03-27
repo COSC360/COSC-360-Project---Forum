99940c24debed7c31ec3daa94d77c274a70137b4
Zikora Dozie <doziezikora@gmail.com>
2023-03-27 00:20:38 -0700

    DB login info update

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-define('DBCONN', "mysql:host=cosc360.ok.ubc.ca;dbname=db_17514019");
+define('DBCONN', "mysql:host=s129.ok.ubc.ca;dbname=db_17514019");
 define('DBUSER',"17514019@localhost");
 define ('DBPASS',"nkemdiche22");
 ?>
